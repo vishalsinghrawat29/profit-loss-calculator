5ece637f7aecd9ab16b5f11ab660c5328383d785
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-09-02 13:19:28 +0530

    make responsive

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ function calculateProfitAndLoss(intial, qunatity, current){
         var loss = (intial - current)*qunatity;
         var lossPercentage = (loss/intial)*100;
         showOutput(`Hey, the loss is ${loss.toFixed(2)} and the percent is ${lossPercentage.toFixed(2)}%`);
-        outputBox.style.color = 'Red';
+        outputBox.style.color = '#FF395E';
     }else if(current > intial){
         // profit logic here
         var profit = (current - intial)*qunatity;
         var profitPercentage = (profit/intial)*100;
         showOutput(`Hey, the profit is ${profit.toFixed(2)} and the percent is ${profitPercentage.toFixed(2)}%`);
-        outputBox.style.color = 'Green';
+        outputBox.style.color = '#0FC28B';
     }else{
         // the rest of the logic
         showOutput(`No pain no gain and no gain no pain`) ;
-        outputBox.style.color = 'black';
+        outputBox.style.color = "#0470DE";
     }
 }
 
